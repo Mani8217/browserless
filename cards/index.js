c2fb5d6d2749ce7393f4afb46e663e71b8cc870f
Mani Shidfar <mreza.shidfar@gmail.com>
2023-01-28 13:34:45 +0100

    new branch cards

---
+++ cards/index.js
@@ -0,0 +1,16 @@
+
+function showPage(page) {
+    document.querySelectorAll('.div').forEach(div => {
+        div.style.display = 'none';
+    });
+    document.querySelector(`#${page}`).style.display = 'block'; 
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+document.querySelectorAll('.page-change').forEach(button => {
+    
+    button.onclick = function() {
+        showPage(this.dataset.page);
+    }
+})
+})
